d28d4e3813daa921c696b6aa01d629bdff18584a
merlinnerd922 <richardchen922@gmail.com>
2022-07-25 18:58:59 -0400

    master : Adding board cube

--- Assets/Scripts/SokobanBoard.cs
+++ Assets/Scripts/SokobanBoard.cs
@@ -6,6 +6,17 @@ public class SokobanBoard : MonoBehaviour
 {
     public void Initialize()
     {
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                GenerateCube();
+            }
+        }
+    }
+
+    private void GenerateCube()
+    {
         
     }
 }
