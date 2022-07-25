83c57a145c2c24c3afca496791701712ed2ef0bc
merlinnerd922 <richardchen922@gmail.com>
2022-07-25 19:37:20 -0400

    master : Setting camera rotation

---
+++ Assets/Scripts/Extend.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public static class Extend
+    {
+        public static bool IsOdd(this int i)
+        {
+            return i % 2 == 1;
+        }
+        
+        public static Bounds GetMaxBounds(this GameObject g) {
+            var b = new Bounds(g.transform.position, Vector3.zero);
+            foreach (Renderer r in g.GetComponentsInChildren<Renderer>()) {
+                b.Encapsulate(r.bounds);
+            }
+            return b;
+        }
+
+        public static void SetHeight(this Camera c, int height)
+        {
+            var transform = c.transform;
+            var transformPosition = transform.position;
+            transform.position = new Vector3(transformPosition.x, height, transformPosition.z);
+        }
+
+        public static void SetRotation(this Camera c, int x, int y, int z)
+        {
+            c.transform.rotation = Quaternion.Euler(x, y, z);
+        }
+    }
+}
--- Assets/Scripts/SokobanBoard.cs
+++ Assets/Scripts/SokobanBoard.cs
@@ -1,24 +1,42 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using DefaultNamespace;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class SokobanBoard : MonoBehaviour
 {
-    public BoardCube boardCubePrefab;
+    private const int BOARD_WIDTH = 10;
+    private const int BOARD_LENGTH = 10;
+
+    [FormerlySerializedAs("boardCubePrefab")] 
+    public BoardCube boardCubeOddPrefab;
+    
+    public BoardCube boardCubeEvenPrefab;
+    public Camera mainCamera;
     
     public void Initialize()
     {
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < BOARD_WIDTH; i++)
         {
-            for (int j = 0; j < 10; j++)
+            for (int j = 0; j < BOARD_LENGTH; j++)
             {
                 GenerateCube(i, j);
             }
         }
+
+        var boardBounds = gameObject.GetMaxBounds();
+
+        mainCamera.SetHeight(10);
+        mainCamera.SetRotation(0, -90, 0);
     }
 
     private void GenerateCube(int i, int j)
     {
-        Instantiate(boardCubePrefab.gameObject, new Vector3(i, 0, j), Quaternion.identity);
+        var original = (i + j).IsOdd() ? boardCubeOddPrefab.gameObject : boardCubeEvenPrefab.gameObject;
+        Instantiate(original, new Vector3(i, 0, j), Quaternion.identity, this.transform);
     }
+
+
 }
