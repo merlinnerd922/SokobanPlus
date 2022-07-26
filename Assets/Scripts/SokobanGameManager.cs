978bf70b847dd3cfceedff63ddd4f6df29e48535
merlinnerd922 <richardchen922@gmail.com>
2022-07-25 20:39:21 -0400

    master : ADding variables for game state

--- Assets/Scripts/SokobanGameManager.cs
+++ Assets/Scripts/SokobanGameManager.cs
@@ -5,10 +5,32 @@ using UnityEngine;
 public class SokobanGameManager : MonoBehaviour
 {
     public SokobanBoard sokobanBoard;
-    
+
+    private SokobanGameState _gameState;
     
     public void Start()
     {
         sokobanBoard.Initialize();
+        StartReceiveInput();
+    }
+
+    private void StartReceiveInput()
+    {
+        StartCoroutine(StartReceivingInput());
+    }
+
+    private IEnumerator StartReceivingInput()
+    {
+        while (true)
+        {
+            yield return null;
+        }
     }
 }
+
+public enum SokobanGameState
+{
+    STATIONARY = 0,
+    IN_MOTION = 1
+}
+
