db59d014093e99c2b968302369b8dbb460dd8472
merlinnerd922 <richardchen922@gmail.com>
2022-08-01 19:35:45 -0400

    master : Adding link to game scene

--- Assets/Scripts/MainMenuButton.cs
+++ Assets/Scripts/MainMenuButton.cs
@@ -1,18 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuButton : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public void GoToGameScene()
     {
-        
+        SceneManager.LoadScene("Scenes/LevelScene");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
